200320d488e25000323f6c613653168cba1276ea
BecooOn <adem.bekci.28@gmail.com>
2024-05-05 01:14:33 +0300

    notfound-page

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-<p>Clarusway<img align="right"
-  src="https://secure.meetupstatic.com/photos/event/3/1/b/9/600_488352729.jpeg"  width="15px"></p>
+  
+  https://shopping-becoo.netlify.app/
 
 # Project-009 : Clarus Shopping (RP-09)
 
--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import { Box, Button } from "@mui/material"
+import { useNavigate } from "react-router-dom"
 
 const NotFound = () => {
+  const navigate = useNavigate()
+
   return (
-    <div>NotFound</div>
+    <Box sx={{"textAlign":"center"}}>
+      <img src="./404.png" alt="404" />
+      <div>
+        <Button sx={{border:"1px solid",mr:"10px"}} onClick={() => navigate("/")}>
+          Home
+        </Button>
+        <Button sx={{border:"1px solid"}} onClick={() => navigate(-1)}>
+          Go Back
+        </Button>
+      </div>
+    </Box>
   )
 }
 
